b42b4397c1c54492ba38e9773867bcf436d6e49a
krillinkills <beingvaibhav858@gmail.com>
2022-07-12 04:07:07 +0530

    gistering user

--- routes/Auth.routes.js
+++ routes/Auth.routes.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const createError = require('http-errors');
+const User = require('../models/User');
 
-router.post('/register', (req, res) => {
-  res.send('register');
+router.post('/register', async (req, res, next) => {
+  try {
+    //check if email and pass present
+    const { email, body } = req.body;
+    if (!email || !password) throw createError.BadRequest();
+
+    //check if user registered
+    const userExists = await User.findOne({ email: email });
+    if (userExists)
+      throw createError.Conflict(`${email}email already registered`);
+
+    //Savig in DB
+    const user = new User({ email: email, password: password });
+    const savedUser = await user.save();
+    res.status(201).send(savedUser);
+  } catch {
+    next(error);
+  }
 });
 
 router.post('/login', (req, res) => {
