f6f351210e2cdedab6cb55d26dfab631bf7d074a
krillinkills <beingvaibhav858@gmail.com>
2022-07-12 20:50:40 +0530

    Hashing pasword

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const UserSchema = mongoose.Schema({
   email: {
     type: String,
@@ -12,4 +13,16 @@ const UserSchema = mongoose.Schema({
   },
 });
 
+//this will run before save
+//this is middleware for mongoose
+UserSchema.pre('save', async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedpass = await bcrypt.hashSync(this.password, salt);
+    this.password = hashedpass;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 module.exports = mongoose.model('User', UserSchema);
